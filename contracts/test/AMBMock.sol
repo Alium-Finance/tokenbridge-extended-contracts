f3d753e867d168b0f7e8002c0a42f23688ddeafb
pavel bolhar <gfijrb@yandex.ua>
2022-04-25 19:14:49 +0300

    update AMB mock relayTokens method

--- contracts/test/AMBMock.sol
+++ contracts/test/AMBMock.sol
@@ -4,8 +4,13 @@ pragma solidity >=0.6.2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+//
+//interface IAMBMock {
+//	function relayTokens(address _receiver, uint256 _value) external;
+//	function relayTokens(address _receiver, address _test, uint256 _value) external;
+//}
 
-contract AMBMock {
+contract AMBMock /*is IAMBMock*/ {
 	event ActionOnTransfer(address receiver);
 
 	IERC20 public token;
@@ -14,7 +19,14 @@ contract AMBMock {
 		token = _token;
 	}
 
-	function relayTokens(address _receiver, uint256 _value) external {
+//	function relayTokens(address _receiver, uint256 _value) external virtual override {
+//		address to = address(this);
+//		token.transferFrom(msg.sender, to, _value);
+//		emit ActionOnTransfer(_receiver);
+//	}
+
+	function relayTokens(address _receiver, address _test, uint256 _value) external {
+		_test;
 		address to = address(this);
 		token.transferFrom(msg.sender, to, _value);
 		emit ActionOnTransfer(_receiver);
